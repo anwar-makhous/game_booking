a125726c4a902515832885a7b1ed3ebe24c15e1c
anwar_makhous <anwar.makhous@gmail.com>
2022-10-08 20:44:29 +0300

    set home page backgroundColor to Colors.white

--- lib/Views/Pages/home_page.dart
+++ lib/Views/Pages/home_page.dart
@@ -20,6 +20,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
